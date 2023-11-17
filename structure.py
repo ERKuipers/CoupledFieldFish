85559240cd536b51330bc6ce957a25c61d5c5da0
ERKuipers <elsrkuipers@gmail.com>
2023-11-17 10:44:29 +0100

    spatial extent and field variables in initial and dynamic

--- structure.py
+++ structure.py
@@ -1,19 +1,24 @@
+#%% importing campio
 import datetime
 import os
 import pcraster as pcr
 import pcraster.framework as pcrfw
-from xugrid import ugrid_rasterize
+import xarray as xr
+import xugrid as xu
+from xugrid_func import ugrid_rasterize
 import campo
 
-seed = 5
-pcr.setrandomseed(seed)
+#%% 
 
 scratch_dir = "C:/Users/els-2/OneDrive - Universiteit Utrecht/Brain/Thesis/campo_tutorial/fish/scratch"
 os.chdir(scratch_dir)
 general = "C:/Users/els-2/OneDrive - Universiteit Utrecht/Brain/Thesis/campo_tutorial/fish"
-maas_extent = "C:/Users/els-2/OneDrive - Universiteit Utrecht/Brain/Thesis/maas_data/maps/water_xy.csv"
-babybulls_pref = 'babybulls_pref.xlsx'
+maas_extent = "C:/Users/els-2/OneDrive - Universiteit Utrecht/Brain/Thesis/maas_data/scratch/water_xy_res5.csv"
+#babybulls_pref = 'babybulls_pref.xlsx'
+map_nc = "C:/Users/els-2/OneDrive - Universiteit Utrecht/Brain/Thesis/maas_data/new_fm_map.nc"
+test_extent = "C:/Users/els-2/OneDrive - Universiteit Utrecht/Brain/Thesis/maas_data/scratch/test_waterextent.csv"
 
+#%% model
 class FishEnvironment(pcrfw.DynamicModel):
 
     def __init__(self):
@@ -45,14 +50,15 @@ class FishEnvironment(pcrfw.DynamicModel):
         # self.fishenv.bulls.add_property_set ('bulls_loc', 'bull_agents.shp')  # --> convert to .csv for campo
         # self.fishenv.bulls.set_epsg(28992)
         self.water = self.fishenv.add_phenomenon ('water') # could we possibly reduce the first step of this? 
-        self.water.add_property_set ('area', maas_extent) # the water area always has the same spatial as well as temporal extent (it always exists)
-        
+        self.water.add_property_set ('area', test_extent) # the water area always has the same spatial as well as temporal extent (it always exists)
+        print ('added the field as a domain') # takes a loooooooooooong time 
+        #self.water.area.flow_velocity
         self.timestep = 0
         # set crs
-  
+        self.water.set_epsg(28992)
         # write the lue dataset
         self.fishenv.write()
-
+       
         # print the run duration
         end = datetime.datetime.now() - init_start
         print(f'init: {end}')
@@ -60,10 +66,10 @@ class FishEnvironment(pcrfw.DynamicModel):
     def dynamic(self):
         start = datetime.datetime.now()
         self.timestep += 1 
-        self.fishenv.water.area.flow_velocity =  ('flow_velocity'+str(self.timestep)+'.tif') # overwriting flow velocity for a specific timestep to 
-        
+        self.water.area.flow_velocity = (ugrid_rasterize (map_nc, 5, self.timestep)).values # properties can have the following shape: np.ndarray, or property.Property, or int or float. 
+        print (self.timestep, 'going to write it now!')
         # add from the surroundings function
-        self.fishenv.bulls.add_property_set ('surroundings', 'age_related_buffer'+str(self.timestep) +'.tif' ) 
+        # self.fishenv.bulls.add_property_set ('surroundings', 'age_related_buffer'+str(self.timestep) +'.tif' ) 
         # call surroundings function and add to bulls phenomenon: likelihood of travel 
         
         self.fishenv.write(self.currentTimeStep())
